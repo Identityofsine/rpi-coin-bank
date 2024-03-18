0b1a8404818a49eca5d1aeff2077c6dd2a1bfb59
fofx <identityofsine@gmail.com>
2024-03-18 14:43:37 -0400

    test

--- gpio.py
+++ gpio.py
@@ -31,7 +31,7 @@ class GPIOSystem():
 		GPIO.setmode(GPIO.BCM)
 		index = 0
 		print(self.pin_sheet)
-		for pin in self.pin_sheet:
+		for pin in self.pin_sheet[0]:
 			pin_json = self.pin_sheet["pin"]
 			pin_number = int(pin_json["pin"])
 			print(f"Setting up pin {pin_number}")
