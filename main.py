68fefe63092e1090f8ab53e0f078979270568e62
fofx <identityofsine@gmail.com>
2024-04-18 19:35:29 -0400

    added api_interface

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import RPi.GPIO as GPIO
 from coinbank import CoinBank 
-from gpio import GPIOAPI, GPIOSystem
-import time
+#from gpio import GPIOAPI, GPIOSystem
+#import time
 
 coinbank = CoinBank()
-x = GPIOAPI(coinbank)
+#x = GPIOAPI(coinbank)
 
 
 
